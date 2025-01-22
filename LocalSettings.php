0fa1d4acd906a5c8c4280e13db5c062d09e929e3
PrabhasXD <74493749+PrabhasDaily@users.noreply.github.com>
2025-01-22 20:24:31 +0530

    Update LocalSettings.php
    Replaced default footer icons with custom ones for jbcstudios.miraheze.org

--- LocalSettings.php
+++ LocalSettings.php
@@ -1750,6 +1750,27 @@ $wgConf->settings += [
 				],
 			],
 		],
+                'jbcstudios' => [
+			'poweredby' => [
+				'mediawiki' => [
+					'src' => 'https://jbcstudios.miraheze.org/File:Poweredbymediawiki_badge.svg',
+					'url' => 'https://www.mediawiki.org/',
+					'alt' => 'Powered by MediaWiki',
+				],
+				'miraheze' => [
+					'src' => 'https://jbcstudios.miraheze.org/File:Miraheze_badge.svg',
+					'url' => 'https://meta.miraheze.org/wiki/Special:MyLanguage/Miraheze_Meta',
+					'alt' => 'Hosted by Miraheze',
+				],
+			],
+			'copyright' => [
+				'copyright' => [
+					'src' => 'https://jbcstudios.miraheze.org/File:Ccbysa_badge.svg',
+					'url' => 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+					'alt' => 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)',
+				],
+			],
+		],
 		'cafewiki' => [
 			'poweredby' => [
 				'mediawiki' => [
